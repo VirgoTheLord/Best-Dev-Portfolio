2b3d38df10d1a71daaf17dc78a190cd1a5914900
Alwin Mathew <alwinaby2004@gmail.com>
2024-08-28 19:43:26 +0530

    Update all files On branch main Initial commit new file:   devfolio/.eslintrc.json new file:   devfolio/.gitignore new file:   devfolio/README.md new file:   devfolio/app/globals.css 	new file:   devfolio/app/layout.tsx 	new file:   devfolio/app/page.tsx 	new file:   devfolio/app/provider.tsx 	new file:   devfolio/components/Approach.tsx 	new file:   devfolio/components/Clients.tsx 	new file:   devfolio/components/Experience.tsx 	new file:   devfolio/components/Footer.tsx 	new file:   devfolio/components/Grid.tsx 	new file:   devfolio/components/Hero.tsx 	new file:   devfolio/components/RecentProjects.tsx 	new file:   devfolio/components/ui/3d-pin.tsx 	new file:   devfolio/components/ui/BentoGrid.tsx 	new file:   devfolio/components/ui/CanvasRevealEffect.tsx 	new file:   devfolio/components/ui/FloatingNav.tsx 	new file:   devfolio/components/ui/Globe.tsx 	new file:   devfolio/components/ui/GradientBg.tsx 	new file:   devfolio/components/ui/GridGlobe.tsx 	new file:   devfolio/components/ui/InfiniteMovingCards.tsx 	new file:   devfolio/components/ui/MagicButton.tsx 	new file:   devfolio/components/ui/MovingBorders.tsx 	new file:   devfolio/components/ui/Spotlight.tsx 	new file:   devfolio/components/ui/TextGenerateEffect.tsx 	new file:   devfolio/data/confetti.json 	new file:   devfolio/data/globe.json 	new file:   devfolio/data/index.ts 	new file:   devfolio/lib/utils.ts 	new file:   devfolio/next.config.mjs 	new file:   devfolio/package-lock.json 	new file:   devfolio/package.json 	new file:   devfolio/postcss.config.mjs 	new file:   devfolio/public/app.svg 	new file:   devfolio/public/appName.svg 	new file:   devfolio/public/arrow.svg 	new file:   devfolio/public/b1.svg 	new file:   devfolio/public/b4.svg 	new file:   devfolio/public/b5.svg 	new file:   devfolio/public/bg.png 	new file:   devfolio/public/c.svg 	new file:   devfolio/public/cloud.svg 	new file:   devfolio/public/cloudName.svg 	new file:   devfolio/public/confetti.gif 	new file:   devfolio/public/dock.svg 	new file:   devfolio/public/dockerName.svg 	new file:   devfolio/public/exp1.svg 	new file:   devfolio/public/exp2.svg 	new file:   devfolio/public/exp3.svg 	new file:   devfolio/public/exp4.svg 	new file:   devfolio/public/fm.svg 	new file:   devfolio/public/footer-grid.svg 	new file:   devfolio/public/git.svg 	new file:   devfolio/public/grid.svg 	new file:   devfolio/public/gsap.svg 	new file:   devfolio/public/host.svg 	new file:   devfolio/public/hostName.svg 	new file:   devfolio/public/insta.svg 	new file:   devfolio/public/jsm-logo.png 	new file:   devfolio/public/link.svg 	new file:   devfolio/public/next.svg 	new file:   devfolio/public/p1.svg 	new file:   devfolio/public/p2.svg 	new file:   devfolio/public/p3.svg 	new file:   devfolio/public/p4.svg 	new file:   devfolio/public/profile.svg 	new file:   devfolio/public/re.svg 	new file:   devfolio/public/s.svg 	new file:   devfolio/public/stream.svg 	new file:   devfolio/public/streamName.svg 	new file:   devfolio/public/tail.svg 	new file:   devfolio/public/three.svg 	new file:   devfolio/public/ts.svg 	new file:   devfolio/public/twit.svg 	new file:   devfolio/public/wha.svg 	new file:   devfolio/tailwind.config.ts 	new file:   devfolio/tsconfig.json

---
+++ devfolio/data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I emphasize client partnership, promoting clear and continuous communication. ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My Tech Stack",
+      description: "I constantly work on Updating...",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "A Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a Chess-AI Model for PVE Chess",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "GEN-AI Resume Processor",
+      des: "Processes resume pdf's and extract them into databases and required csv's in a click.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/VirgoTheLord/Gen-AI-Resume-Processor",
+    },
+    {
+      id: 2,
+      title: "Yoom - Video Conferencing App",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/VirgoTheLord/Gen-AI-Resume-Processor",
+    },
+    {
+      id: 3,
+      title: "AI Image SaaS - Canva Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/VirgoTheLord/Gen-AI-Resume-Processor",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/VirgoTheLord/Gen-AI-Resume-Processor",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Yeah, He do be working fine. I mean, not as much as me, but ok. Work wise, He gets shit done. Worth a shot (P.S Please recruit this guy, i get commission.)",
+      name: "Blessen Eby",
+      title: "Director of Rizz",
+    },
+    {
+      quote:
+        "Elixir K.., sorry, going out of line. Decent Payyan, Get him a job. He needs the money to go out with us regularly. Boy too busy nowadays.",
+      name: "David Mojish",
+      title: "Director of React(He the Boss, ngl)",
+    },
+    {
+      quote:
+        "Onnum Parayanilla. Set Payyan. Please Recruit Instantly.",
+      name: "Rithik Rajan Baby",
+      title: "Director of... you guessed it.. PussyKuttan and Gang",
+    },
+    {
+      quote:
+        "See, He does ok work. I guess giving him a chance wont hurt. Also, provide me one as well because of course, I am his mentor. You get the assignment, yeah? ",
+      name: "Abhiram Sabu",
+      title: "Director of ShortKings, no offence as usual",
+    },
+    {
+      quote:
+        "Alwin is a really good guy. If there is anyone around that I would recommend for a job, It will be him. I cannot think of any person more capable than him around. I certify all these with complete dishonesty.",
+      name: "Gokul Krishna",
+      title: "Director of Perfectio",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "GEN-AI and Prompt Engineering Intern",
+      desc: "Assisted in creating a Resume Processor, by implementing an llm. ",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Web App Dev - Bus Pass",
+      desc: "Designed and developed Web app for providing a Bus Pass for infrequent bus users. ",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Chess-AI Model: Hobby Dev",
+      desc: "Developing a Chess-AI model for a PVE- Chess game experience.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Image Generation: Pic to Pic/ Text to Pic",
+      desc: "Currently trying to develop an Image generation model that can work both disciplines.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
