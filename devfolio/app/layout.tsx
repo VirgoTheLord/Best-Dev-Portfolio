832e4907e5a99dc052937011b9609e2176dc9208
Alwin Mathew <alwinaby2004@gmail.com>
2024-09-04 23:19:05 +0530

    modified:   devfolio/.gitignore 	modified:   devfolio/app/layout.tsx 	modified:   devfolio/next.config.mjs 	modified:   devfolio/package-lock.json 	modified:   devfolio/package.json 	new file:   devfolio/public/manifest.json

--- devfolio/app/layout.tsx
+++ devfolio/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Alwin's Devfolio",
   description: "Modern and Minimalistic approach to Folio's.",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
--- devfolio/next.config.mjs
+++ devfolio/next.config.mjs
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from "next-pwa";
+import { GenerateSW } from "workbox-webpack-plugin";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer && process.env.NODE_ENV === "production") {
+      config.plugins.push(
+        new GenerateSW({
+          clientsClaim: true,
+          skipWaiting: true,
+        })
+      );
+    }
+    return config;
+  },
+};
+
+const pwaConfig = {
+  dest: "public",
+  //   disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+};
+
+export default withPWA(pwaConfig)(nextConfig);
